1bee8f0f9e76286edbd85f83ce5fea913a21b594
roaksey <roaksey@outlook.com>
2024-10-21 12:55:11 +0545

    Connection String with index page addition

--- TestPortal.Web/Controllers/HomeController.cs
+++ TestPortal.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TestPortal.Web.Models;
+using TestPortal.Web.Models.ViewModel;
 
 namespace TestPortal.Web.Controllers
 {
@@ -15,7 +16,23 @@ namespace TestPortal.Web.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var model = new PopulationVM
+            {
+                PopulationTypes = new List<Population>
+            {
+                new Population { PopulationType = "Old", Number = "1,000,000,000" },
+                new Population { PopulationType = "Young", Number = "3,000,000,000" },
+                new Population { PopulationType = "Child", Number = "1,000,000,000" }
+            },
+                TopCountries = new List<CountryPopulation>
+            {
+                new CountryPopulation { SN = 1, CountryName = "China", Population = 152220000 },
+                new CountryPopulation { SN = 2, CountryName = "India", Population = 85000000 },
+                new CountryPopulation { SN = 3, CountryName = "USA", Population = 6663441 }
+            }
+            };
+
+            return View(model);
         }
 
         public IActionResult Privacy()
